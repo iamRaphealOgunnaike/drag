770f20cdc20305c7266542b1495cb661d1f88a8b
ogunraph_lab <scitech4africa@gmail.com>
2022-11-14 08:39:35 -0800

    second commit

---
+++ dragdrop2/src/column.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export default class column extends React.Component {
+ 
+
+  render() {
+    return this.props.column.title;
+  }
+}
---
+++ dragdrop2/src/initial-data.js
@@ -0,0 +1,22 @@
+const initialData = {
+    tasks:{
+        "task-1": {id:'task-1', content: "Take out the garbage"},
+        "task-2": {id:'task-2', content: "watch your favorite show"},
+        "task-3": {id:'task-3', content: "charge my phone"},
+        "task-4": {id:'task-4', content: "Cook dinner"},
+        "task-5": {id:'task-5', content: "rapheals additional items"},
+    },
+    columns:{
+        'column-1':{
+            id:'column-1', 
+            title:'To -do', 
+            taskids:['task-1','task-2','task-3','task-4','task-5'],
+        },
+    },
+
+     // facilite reordering of the columns
+     columnOrder: ['column-1'],    
+       
+};
+
+export default initialData;
